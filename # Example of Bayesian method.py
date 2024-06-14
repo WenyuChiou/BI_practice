a4bfdddd9412a11721586490d65abe291be14cae
Wenyu Chiou <wenyuchiou12@gmail.com>
2024-06-14 21:40:21 +0800

    add the 1st and 2nd step for inpection of paramter within priors

--- # Example of Bayesian method.py
+++ # Example of Bayesian method.py
@@ -11,29 +11,91 @@
 
 """
 import numpy as np # type: ignore
-import statistics 
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import pandas as pd
+import os 
+import random
 
 #Step 2: Framing statistic model
 
-def stat_model(g,ey):
-    g,ey = np.array(g), np.array(ey) # gender and eductaion year [yr]
+def model(g,ey,coeff):  
+    """ 
+    Model: salary = Beta0 + Beta1 * gender + Beta2 * education year
+    
+    Input: 
+        g,ey = np.array(g), np.array(ey) # gender and eductaion year [yr]
+    
+    Parameter:
+    """
     sl = np.zeros(len(g))       # forecast salary 
-
     #---regression model---
     # Coefficient of model 
-    Beta0 = 1 
-    Beta1 = 2
-    Beta2 = 3
+    Beta0 = coeff['B0']
+    Beta1 = coeff['B1']
+    Beta2 = coeff['B2']
 
     for ii in range(len(g)):
         sl[ii] = Beta0 + Beta1*g[ii] + Beta2*ey[ii]
         
-    std = statistics.variance(sl)
+    #Plot the histogram of estimated salary levels
     
-    print(f'stdev: {std}')
-    return sl, std
+    # fig, ax1 = plt.subplots(figsize=(10, 5))
+   
+    # ax1.hist( sl,  
+    #       bins=np.arange(sl.min(), sl.max()+1000, 1000), 
+    #       rwidth=0.5 )
+    
+    # ax1.set_xlabel('X')
+    # ax1.set_ylabel('Probability Density')
+    
+    #Calcualted the mean and standard deviation of estimated salary levels
+    mu, std = norm.fit(sl)
+       
+    return sl, std, mu
 
-test ,st = stat_model([1,1,1,1,0],[20,10,15,18,17])
+dir = r'C:\Users\user\Desktop\Lehigh\pre-phd\BayesianStatistics_HawjengChiou\Ch01'
+
+
+# g = [random.randint(1,40) for _ in range(100)] #random seeds for gender data
+# ey = [random.randint(0,1) for _ in range(100)] #random seeds for education years
+
+# Observed data
+obs_data = pd.read_csv(os.path.join(dir,'data1.csv'),header=None)
+g, ey = obs_data.iloc[:,1], obs_data.iloc[:,3]
+sl = obs_data.iloc[:,4]
+
+model_data, std1, mu1 = model(g,ey,{'B0':28,'B1':8.4,'B2':3.5})
+
+#Step 2: Specification and Inspection of parameter (coefficient) in priors
+
+# Define acceptance ratio
+def lnlike(obs, model, yerr):
+
+    Lnlike = 0
+    for i in range(len(obs)):
+        Lnlike += -1/2*((obs - model)/yerr)**2  
+    
+    return Lnlike
+
+def lnprior(coeff):
+    """ Check the coefficient within the priors"""
+    Beta0 = coeff['B0']
+    Beta1 = coeff['B1']
+    Beta2 = coeff['B2']
+    
+    if Beta0 > 0: 
+        return 0.0
+    else:
+        return -np.inf
+
+def lnprob(coeff,obs, model, yerr):
+    lp = lnprior(coeff)
+    if lp != 0.0:
+        return -np.inf
+    return lp + lnlike(coeff, obs, model, yerr) #recall if lp not -inf, its 0, so this just returns likelihood
+
+
 
 
 
