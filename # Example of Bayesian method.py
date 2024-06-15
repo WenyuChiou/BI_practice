81e6c3fc52dea14d7874dd89fbcec70f01936500
Wenyu Chiou <wenyuchiou12@gmail.com>
2024-06-15 21:00:21 +0800

    Modified the function of modeling setting

--- # Example of Bayesian method.py
+++ # Example of Bayesian method.py
@@ -14,26 +14,30 @@ import numpy as np # type: ignore
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import pandas as pd
+import emcee
 import os 
 import random
 
 #Step 2: Framing statistic model
 
-def model(g,ey,coeff):  
+def model(coeff):  
     """ 
-    Model: salary = Beta0 + Beta1 * gender + Beta2 * education year
+    Model: salary = Beta0 + Beta1 * gender + Beta2 * education year 
     
     Input: 
         g,ey = np.array(g), np.array(ey) # gender and eductaion year [yr]
     
     Parameter:
     """
-    sl = np.zeros(len(g))       # forecast salary 
+    dir = r'C:\Users\user\Desktop\Lehigh\pre-phd\BayesianStatistics_HawjengChiou\Ch01'
+    
+    obs_data = pd.read_csv(os.path.join(dir,'data1.csv'),header=None)
+    g, ey = obs_data.iloc[:,1], obs_data.iloc[:,3]
+    sl = np.zeros(len(g))       # forecast salary
+    
     #---regression model---
     # Coefficient of model 
-    Beta0 = coeff['B0']
-    Beta1 = coeff['B1']
-    Beta2 = coeff['B2']
+    Beta0, Beta1, Beta2 = coeff
 
     for ii in range(len(g)):
         sl[ii] = Beta0 + Beta1*g[ii] + Beta2*ey[ii]
@@ -52,7 +56,7 @@ def model(g,ey,coeff):
     #Calcualted the mean and standard deviation of estimated salary levels
     mu, std = norm.fit(sl)
        
-    return sl, std, mu
+    return sl
 
 dir = r'C:\Users\user\Desktop\Lehigh\pre-phd\BayesianStatistics_HawjengChiou\Ch01'
 
@@ -62,38 +66,55 @@ dir = r'C:\Users\user\Desktop\Lehigh\pre-phd\BayesianStatistics_HawjengChiou\Ch0
 
 # Observed data
 obs_data = pd.read_csv(os.path.join(dir,'data1.csv'),header=None)
-g, ey = obs_data.iloc[:,1], obs_data.iloc[:,3]
 sl = obs_data.iloc[:,4]
 
-model_data, std1, mu1 = model(g,ey,{'B0':28,'B1':8.4,'B2':3.5})
 
 #Step 2: Specification and Inspection of parameter (coefficient) in priors
 
 # Define acceptance ratio
-def lnlike(obs, model, yerr):
-
-    Lnlike = 0
-    for i in range(len(obs)):
-        Lnlike += -1/2*((obs - model)/yerr)**2  
-    
-    return Lnlike
+def lnlike(theta, y, yerr):
+    return -0.5 * np.sum(((y - model(theta))/yerr) ** 2)
 
 def lnprior(coeff):
     """ Check the coefficient within the priors"""
-    Beta0 = coeff['B0']
-    Beta1 = coeff['B1']
-    Beta2 = coeff['B2']
+    Beta0, Beta1, Beta2 = coeff
     
     if Beta0 > 0: 
         return 0.0
     else:
         return -np.inf
 
-def lnprob(coeff,obs, model, yerr):
-    lp = lnprior(coeff)
-    if lp != 0.0:
+def lnprob(theta, y, yerr):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
         return -np.inf
-    return lp + lnlike(coeff, obs, model, yerr) #recall if lp not -inf, its 0, so this just returns likelihood
+    return lp + lnlike(theta, y, yerr)
+
+# Step 3: Implementation of estimating the parameter
+
+#Initnial Data and Setting
+
+Terr = 0.05*np.mean(sl)
+data = (sl, Terr)
+nwalkers = 128
+niter = 500
+initial = np.array([28,8.4,3.5])
+ndim = len(initial)
+p0 = [np.array(initial) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
+
+def main(p0,nwalkers,niter,ndim,lnprob,data):
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=data)
+
+    print("Running burn-in...")
+    p0, _, _ = sampler.run_mcmc(p0, 100)
+    sampler.reset()
+
+    print("Running production...")
+    pos, prob, state = sampler.run_mcmc(p0, niter)
+
+    return sampler, pos, prob, state
+
+sampler, pos, prob, state = main(p0,nwalkers,niter,ndim,lnprob,data)
 
 
 
