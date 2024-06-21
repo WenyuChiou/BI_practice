87843280b81a337bba564757560cd12d2a8cc6e6
Wenyu Chiou <wenyuchiou12@gmail.com>
2024-06-21 23:24:17 +0800

    New example for BI (MH algorithm)

---
+++ # Example of Bayesian 3rd.py
@@ -0,0 +1,12 @@
+# Third example of Bayesian Inference
+# By Wenyu Chiou 
+
+""" 
+The algorithm used in this example is Metropolis-Hastings, which is ofthen used on Bayesian Inference to determine
+whether the new values should be accepted or reserve the current value for calculating posteriors.
+
+
+link: https://bayesiancomputationbook.com/markdown/chp_01.html
+    
+    """
+    
