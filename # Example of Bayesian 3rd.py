c43583676856e0bf302946f54a599301856e2706
Wenyu Chiou <wenyuchiou12@gmail.com>
2024-06-23 22:38:46 +0800

    update definition of posterior

--- # Example of Bayesian 3rd.py
+++ # Example of Bayesian 3rd.py
@@ -3,10 +3,27 @@
 
 """ 
 The algorithm used in this example is Metropolis-Hastings, which is ofthen used on Bayesian Inference to determine
-whether the new values should be accepted or reserve the current value for calculating posteriors.
+whether the new values (forecastes) should be accepted or reserve the current value for calculating posteriors.
+
+Note: MH algorithm is not an optimization method.
 
 
 link: https://bayesiancomputationbook.com/markdown/chp_01.html
     
     """
     
+from scipy import stats
+import numpy as np
+
+
+# definition of posterior distribution given unknown prior distribution and likelihood function
+def post(theta, T, alpha = 1, beta = 1):
+    if (0 <= theta <= 1):
+        prior = stats.beta(alpha, beta).pdf(theta)
+        like = stats.bernoulli(theta).pmf(T).prod()
+        prob = prior * like
+    else:
+        prob = -np.inf
+    return prob
+        
+
